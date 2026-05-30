bf4a02887bbeabd55f2ece918637eaafde02ccfe
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-05-30 20:59:45 +0530

    Standard Input Output Using C++

---
+++ input_output.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int i;
+    long long ll;
+    float f;
+    double d;
+    char c;
+    string word;
+    string line;
+
+    cin >> i;
+    cout << i << '\n';
+
+    cin >> ll;
+    cout << ll << '\n';
+
+    cin >> f;
+    cout << f << '\n';
+
+    cin >> d;
+    cout << d << '\n';
+
+    cin >> c;
+    cout << c << '\n';
+
+    cin >> word;
+    cout << word << '\n';
+
+    cin.ignore();
+
+    getline(cin, line);
+    cout << line << '\n';
+
+    return 0;
+}
